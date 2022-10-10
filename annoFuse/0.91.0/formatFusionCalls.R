82ce5adf1c01d861d40cb8022bca7d46ba3c6b18
Saksham Phul <phulsaksham2@gmail.com>
2022-10-10 11:01:14 -0400

    Feature/annofuse:v0.91.0 (#137)
    * :constuction: initial commit using PR branch
    
    * :sparkle: add data.table library
    
    * :whale: update dockerfile to sue new annofuse version
    
    * :truck: add missing R files
    
    * add updated config.yml
    
    * Update the code based on feedback
    
    Co-authored-by: Alex Sickler <sicklera@chop.edu>

---
+++ annoFuse/0.91.0/formatFusionCalls.R
@@ -0,0 +1,36 @@
+suppressPackageStartupMessages(library("readr"))
+suppressPackageStartupMessages(library("dplyr"))
+suppressPackageStartupMessages(library("optparse"))
+
+
+option_list <- list(
+  make_option(c("-f", "--fusionfile"),type="character",
+              help="Fusion calls from [STARfusion | Arriba]"),
+  make_option(c("-c", "--caller"), type="character",
+              help="starfusion|arriba"),
+  make_option(c("-t", "--tumorid"), type="character",
+              help="KF tumor id"),
+  make_option(c("-o","--outputfile"),type="character",
+              help="Formatted fusion calls from [STARfusion | Arriba] (.TSV)")
+)
+
+# Get command line options, if help option encountered print help and exit,
+opt <- parse_args(OptionParser(option_list=option_list))
+inputfile <- opt$fusionfile
+caller <- opt$caller
+tumorid <- opt$tumorid
+outputfile <- opt$outputfile
+
+
+
+if (caller=="starfusion"){
+  df<-read_tsv(inputfile)
+}
+
+if (caller=="arriba"){
+  df<-read_tsv(inputfile,col_types = readr::cols(breakpoint1 = readr::col_character(),breakpoint2 = readr::col_character()))
+  df$`gene1--gene2`<-paste(df$`#gene1`,df$gene2,sep="--")
+}
+
+# write to output file
+write.table(df,outputfile,sep="\t",quote=FALSE,row.names = FALSE)
