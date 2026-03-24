934b0cb2f7eb7b35d78cb46bbcc112716ddf9b78
Miguel Brown <miguel.a.brown@gmail.com>
2026-03-24 19:56:52 +0000

    :whale: add custom compiled samtools

---
+++ samtools/1.20-multi-arch/link_samtools.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Pick samtools version based on march. Default to generic build if custom build not found
+root="/opt/samtools-1.20"
+march=`gcc -march=native -Q --help=target | grep march= | head -n1 | cut -f3-`
+echo $march
+if [ -d "$root-$march" ]; then
+    export PATH=$PATH:$root-$march/bin
+else
+    export PATH=$PATH:$root/bin
+fi
